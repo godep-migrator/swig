88438c21ca6ab8389398dfecacc11348400a7f34
Jeff Chao <jeffchao@me.com>
2014-03-09 20:27:12 -0700

    Add skeleton for kafka producer on intake.

--- handlers/intake/intake.go
+++ handlers/intake/intake.go
@@ -3,17 +3,18 @@ package intake
 import (
 	"github.com/codegangsta/martini"
 	"github.com/codegangsta/martini-contrib/render"
-	//"github.com/Shopify/sarama"
+	kafka "github.com/Shopify/sarama"
 	"log"
 	"net/http"
 )
 
-func Create(req *http.Request, res http.ResponseWriter, params martini.Params, logger *log.Logger, r render.Render) {
+func Create(producer *kafka.Producer, req *http.Request, res http.ResponseWriter, params martini.Params, logger *log.Logger, r render.Render) {
 	logger.Println(params)
+  logger.Println(producer)
 
 	err := req.ParseForm()
 	if err != nil {
-		logger.Panic(err)
+		logger.Println(err)
 	}
 
 	logger.Println(req.Form)
